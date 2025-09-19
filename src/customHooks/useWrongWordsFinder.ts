ef97987ad56a3e179602a064cacfa9502b946228
REDAELMBARKI <redaelmbarki01@icloud.com>
2025-09-19 23:06:12 +0100

    underlining small ishue ficx last update of it

--- src/customHooks/useWrongWordsFinder.ts
+++ src/customHooks/useWrongWordsFinder.ts
@@ -23,15 +23,25 @@ export const useWrongWordsFinder = ({currentLetter , currentText , setWrongWords
           if(e.key !== ' ') return ;
           //  we get the last index of the char of the previous word with currentLetter.index - 1
           //  we search for the first char index of the word after space 
-           let wordFirstIndex:number;
+           let wordFirstIndex:number = currentLetter.index - 1;
            let i:number = currentLetter.index - 1 ;
            while(currentText[i] !== ' ' && i >= 0 ){
              wordFirstIndex = i ;
             i-- ;
            }
            // we splice the word starting from first index till currentLetter.index since slice exclude the last the last number we put currentLetter.index istead of currentLetter.index - 1
-           console.log('storing obj')
-           setWrongWords(prev => [...prev , {start: wordFirstIndex, end : currentLetter.index - 1}])
+            
+            let isInWrongChars:boolean = false ;
+            for(let i = wordFirstIndex ; i <=  currentLetter.index - 1 ; i++){
+                 if(wrongChars.includes(i)){
+                   isInWrongChars  =  true ;
+                   break ;
+                 }
+            }
+
+            if(isInWrongChars){
+               setWrongWords(prev => [...prev , {start: wordFirstIndex, end : currentLetter.index - 1}])
+            }
     
         }
         window.addEventListener('keydown' , asignPreviousWordAsWrong)
